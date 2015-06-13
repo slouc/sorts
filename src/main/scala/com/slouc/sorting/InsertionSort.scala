9363dd5995be79ff630e34750b7c54f01c44a0d7
slouc <sinisalouc@gmail.com>
2015-06-13 17:36:11 +0200

    improved InsertionSort

--- src/main/scala/com/slouc/sorting/InsertionSort.scala
+++ src/main/scala/com/slouc/sorting/InsertionSort.scala
@@ -13,18 +13,18 @@ class InsertionSort extends Sort {
   def sort(list: List[Int]): List[Int] = {
     
     def solve(list1: List[Int], list2: List[Int]): (List[Int], List[Int]) = {
-      if (list2.size == 1) (list1, list2)
+      if (list2 == Nil) (list1, list2)
       else (insert(list2.head, list1), list2.tail)
     }
     
     def insert(elem: Int, list: List[Int]): List[Int] = (elem :: list).sorted
 
     def iterate(list1: List[Int], list2: List[Int]): List[Int] = {
-      if (list2.size == 1) list1 ::: list2
+      if (list2 == Nil) list1
       else {
         val (newList1, newList2) = solve(list1, list2)
-        val onePass = newList1 ::: newList2
-        if (isSorted(onePass)) onePass
+        if (newList2 == Nil || isSorted(newList2) && (newList2.head > newList1.last)) 
+          newList1 ::: newList2
         else iterate(newList1, newList2)
       }
     }
