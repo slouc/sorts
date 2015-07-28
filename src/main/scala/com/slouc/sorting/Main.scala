e402233c714aca1f09918488ea1dd3c0a036533d
slouc <sinisalouc@gmail.com>
2015-07-28 19:05:09 +0200

    simplified insertion sort

--- src/main/scala/com/slouc/sorting/Main.scala
+++ src/main/scala/com/slouc/sorting/Main.scala
@@ -1,6 +1,9 @@
 package com.slouc.sorting
 
-import com.slouc.sorting.sort._
+import scala.math.BigInt.probablePrime
+import scala.util.Random
+import com.slouc.sorting.sort.MergeSort
+import com.slouc.sorting.sort.InsertionSort
 
 /**
  * @author slouc
@@ -8,12 +11,11 @@ import com.slouc.sorting.sort._
  */
 object Main extends App {
 
-  val sort: Sort = new MergeSort
+  val sort: Sort = new InsertionSort
   
   val init = Common.gen
   val solution = sort.sort(init)
   
   println(init)
   println(solution)
-  
 }
--- src/main/scala/com/slouc/sorting/sort/InsertionSort.scala
+++ src/main/scala/com/slouc/sorting/sort/InsertionSort.scala
@@ -10,23 +10,14 @@ import com.slouc.sorting.Sort
  */
 class InsertionSort extends Sort {
 
-  private def solve(list1: List[Int], list2: List[Int]): (List[Int], List[Int]) = {
-    if (list2 == Nil) (list1, list2)
-    else (insert(list2.head, list1), list2.tail)
+  private def insert(elem: Int, list: List[Int]): List[Int] = {
+    if (list == Nil || elem < list.head) elem :: list
+    else list.head :: insert(elem, list.tail)
   }
 
-  private def insert(elem: Int, list: List[Int]): List[Int] = (elem :: list).sorted
-
-  private def iterate(list1: List[Int], list2: List[Int]): List[Int] = {
-    if (list2 == Nil) list1
-    else {
-      val (newList1, newList2) = solve(list1, list2)
-      if (newList2 == Nil || isSorted(newList2) && (newList2.head > newList1.last))
-        newList1 ::: newList2
-      else iterate(newList1, newList2)
-    }
+  def sort(list: List[Int]): List[Int] = {
+    if (list == Nil) Nil
+    else insert(list.head, sort(list.tail))
   }
-
-  def sort(list: List[Int]): List[Int] = iterate(List(list.head), list.tail)
 
 }
